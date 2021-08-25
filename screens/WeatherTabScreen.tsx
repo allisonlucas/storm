359ffd80093483fa07e9a4fedace3c2debe76ba2
Allison Lucas <allisonlucas@Allisons-MacBook-Pro-2.local>
2021-08-25 16:01:12 -0600

    Add expo-location dependency
    Use phone current lat/lng in call to openweathermap api
    Display timezone location and current temp

--- screens/WeatherTabScreen.tsx
+++ screens/WeatherTabScreen.tsx
@@ -1,22 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { OPEN_WEATHER_MAP_API_KEY } from '../utils/apiKeys';
 
 export default function WeatherTabScreen() {
-  useEffect((lat = 25, lon = 25) => {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${OPEN_WEATHER_MAP_API_KEY}&units=imperial`)
-      .then(res => res.json())
-      .then(json => {
-        console.log('json: ', json);
-      });
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        // TODO: Prompt user to input a location
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${OPEN_WEATHER_MAP_API_KEY}&units=imperial`)
+        .then(res => res.json())
+        .then(json => {
+          console.log('json: ', json);
+          setWeatherData(json);
+        });
+    })();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      {/* TODO: Use Google Maps geolocation to turn the lat/lng into a city name */}
+      <Text style={styles.title}>{weatherData?.timezone ? weatherData?.timezone : null}</Text>
+      <Text style={styles.title}>{weatherData?.current?.temp ? `${weatherData?.current?.temp}\u00b0 F` : 'Loading...'}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/WeatherTabScreen.tsx" />
     </View>
