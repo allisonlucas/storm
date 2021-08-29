cf509cf2162d86cebc7d1784c634726a3d7f9117
Allison Lucas <allisonlucas@Allisons-MacBook-Pro-2.local>
2021-08-29 16:38:36 -0600

    Add react-native-chart-kit and first chart
    Add line chart to weather screen displaying temp for hours left in the day
    Add ScrollView to weather screen to make scrolling possible

--- screens/WeatherTabScreen.tsx
+++ screens/WeatherTabScreen.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 import * as Location from 'expo-location';
+import { LineChart } from 'react-native-chart-kit';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { OPEN_WEATHER_MAP_API_KEY } from '../utils/apiKeys';
 
+type OpenWeatherOneCallData = {
+  current: Object;
+  hourly: [
+    {
+      dt: number;
+      temp: number;
+    }
+  ]
+};
+
 export default function WeatherTabScreen() {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState<OpenWeatherOneCallData | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -29,20 +40,56 @@ export default function WeatherTabScreen() {
   }, []);
 
   if (weatherData) {
+    const hoursLeftToday = 24 - new Date(weatherData.hourly[0].dt * 1000).getHours();
+    const hourLabels = weatherData.hourly.map(hourData => new Date(hourData.dt * 1000).getHours().toString()).splice(0, hoursLeftToday);
+    const hourlyTempData = weatherData.hourly.map(hourData => Math.round(hourData.temp)).splice(0, hoursLeftToday);
+
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.weatherIcon}
-          source={{ uri: `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png` }}
-        />
-        <Text style={styles.title}>{`${weatherData.current.weather[0].main}, feels like ${weatherData.current.feels_like}\u00b0 F`}</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <ScrollView style={styles.scrollContainer}>
+          <Image
+            style={styles.weatherIcon}
+            source={{ uri: `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png` }}
+          />
+          <Text style={styles.title}>{`${weatherData.current.weather[0].main}, feels like ${weatherData.current.feels_like}\u00b0 F`}</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
-        {/* TODO: Use Google Maps geolocation to turn the lat/lng into a city name */}
-        <Text style={styles.title}>{weatherData.timezone}</Text>
-        <Text style={styles.title}>{`${weatherData.current?.temp}\u00b0 F`}</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        <EditScreenInfo path="/screens/WeatherTabScreen.tsx" />
+          {/* TODO: Use Google Maps geolocation to turn the lat/lng into a city name */}
+          <Text style={styles.title}>{weatherData.timezone}</Text>
+          <Text style={styles.title}>{`${weatherData.current?.temp}\u00b0 F`}</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+          <LineChart
+            data={{
+              labels: hourLabels,
+              datasets: [
+                {
+                  data: hourlyTempData,
+                },
+              ],
+            }}
+            width={Dimensions.get('window').width - 20}
+            height={220}
+            yAxisSuffix={`\u00b0`}
+            chartConfig={{
+              backgroundColor: '#1cc910',
+              backgroundGradientFrom: '#eff3ff',
+              backgroundGradientTo: '#efefef',
+              decimalPlaces: 0, // optional, defaults to 2dp
+              color: (opacity = 255) => `rgba(0, 0, 0, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+            }}
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+          />
+
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <EditScreenInfo path="/screens/WeatherTabScreen.tsx" />
+        </ScrollView>
       </View>
     );
   }
@@ -54,6 +101,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollContainer: {
+    marginHorizontal: 20,
   },
   title: {
     fontSize: 20,
